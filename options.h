6605e7cd223a820b45f45ebb2238c98256f747df
Alex Donath <a.donath@zfmk.de>
2016-10-21 16:56:33 +0200

    Removed license from files

--- options.h
+++ options.h
@@ -1,31 +1,9 @@
 /**
- *  last changed Time-stamp: <2016-10-21 15:07:07 adonath>
+ *  last changed Time-stamp: <2016-10-21 16:51:37 adonath>
  *
  *  Author: Wolfgang Otto
  *  Modified by: Alexander Donath, a <dot> donath <at> zfmk <dot> de
- *
  */
-///////////////////////////////////////////////////////////////////////////
-//                                                                       //
-// Copyright (C) 2008-2016 by Wolfgang Otto                              //
-// All Rights Reserved.                                                  //
-//                                                                       //
-// Permission to use, copy, modify, and distribute this                  //
-// software and its documentation for NON-COMMERCIAL purposes            //
-// and without fee is hereby granted provided that this                  //
-// copyright notice appears in all copies.                               //
-//                                                                       //
-// THE AUTHOR AND PUBLISHER MAKE NO REPRESENTATIONS OR                   //
-// WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER              //
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE                  //
-// IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A                  //
-// PARTICULAR PURPOSE, OR NON-INFRINGEMENT. THE AUTHORS                  //
-// AND PUBLISHER SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED            //
-// BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING           //
-// THIS SOFTWARE OR ITS DERIVATIVES.                                     //
-//                                                                       //
-///////////////////////////////////////////////////////////////////////////
-                                                             
 
 #ifndef ___WOL_OPTIONS_HPP___
 #define ___WOL_OPTIONS_HPP___
--- utils.cpp
+++ utils.cpp
@@ -1,29 +1,8 @@
 /**
- *  last changed Time-stamp: <2016-10-21 15:05:04 adonath>
+ *  last changed Time-stamp: <2016-10-21 16:52:34 adonath>
  *
  *  Author: Alexander Donath, a <dot> donath <at> zfmk <dot> de
- *
  */
-///////////////////////////////////////////////////////////////////////////
-//                                                                       //
-// Copyright (C) 2010-2016 by Alexander Donath                           //
-// All Rights Reserved.                                                  //
-//                                                                       //
-// Permission to use, copy, modify, and distribute this                  //
-// software and its documentation for NON-COMMERCIAL purposes            //
-// and without fee is hereby granted provided that this                  //
-// copyright notice appears in all copies.                               //
-//                                                                       //
-// THE AUTHOR AND PUBLISHER MAKE NO REPRESENTATIONS OR                   //
-// WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER              //
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE                  //
-// IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A                  //
-// PARTICULAR PURPOSE, OR NON-INFRINGEMENT. THE AUTHORS                  //
-// AND PUBLISHER SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED            //
-// BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING           //
-// THIS SOFTWARE OR ITS DERIVATIVES.                                     //
-//                                                                       //
-///////////////////////////////////////////////////////////////////////////
 
 #include <cstdlib>
 #include <fstream>
